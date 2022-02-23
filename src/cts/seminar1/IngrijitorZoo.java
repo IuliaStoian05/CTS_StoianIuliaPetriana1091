5d1fbc0061734cf76dd020c1d0db293baf95dc3e
IuliaStoian05 <iuliastoian888@gmail.com>
2022-02-23 18:55:16 +0200

    commit

---
+++ src/cts/seminar1/IngrijitorZoo.java
@@ -0,0 +1,21 @@
+package cts.seminar1;
+
+public class IngrijitorZoo {
+    private String nume;
+
+    public IngrijitorZoo(String nume) {
+        this.nume = nume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public void hranesteAnimal(Animal animal, String mancare){
+        animal.mananca(mancare);
+    }
+}
