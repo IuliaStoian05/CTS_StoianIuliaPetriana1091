e942fdb95b098ec5bafe53c62ecaba01bdb5ac05
IuliaStoian05 <72345006+IuliaStoian05@users.noreply.github.com>
2022-04-13 01:56:47 +0300

    Delete src/cts directory

--- src/cts/seminar2/Reader.java
+++
@@ -1,9 +0,0 @@
-package cts.seminar2;
-
-
-import java.io.FileNotFoundException;
-import java.util.List;
-
-public interface Reader {
-    public List<Aplicant> readAplicanti(String file) throws FileNotFoundException;
-}
